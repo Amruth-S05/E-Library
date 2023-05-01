03a90d3fe501ef7030eed34bf4ddaad7a27a5c86
Amruth S <amruth.s05@gmail.com>
2023-05-01 16:50:35 +0530

    App factory initialized

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from .config import Config
+
+from .db import init_db_command
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)  # using config settings from config.py
+
+    @app.route('/')
+    def hello():
+        return "Hello"
+
+    app.cli.add_command(init_db_command)  # adding cli command to initialize db
+
+    return app
